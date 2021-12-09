d51d02e395f8b85fd3b44ff1a3e0797a6d64ae56
nicolasrosal98 <66834619+nicolasrosal98@users.noreply.github.com>
2021-12-09 15:03:25 +0000

    Update query to create table
    Add length constraint to paste_body

--- table.sql
+++ table.sql
@@ -3,5 +3,5 @@ DROP TABLE IF EXISTS pastes;
 CREATE TABLE pastes (
   id SERIAL PRIMARY KEY,
   paste_title TEXT NOT NULL,
-  paste_body TEXT NOT NULL
-  );
+  paste_body TEXT NOT NULL CONSTRAINT length CHECK (char_length(paste_body) > 0)
+ );
